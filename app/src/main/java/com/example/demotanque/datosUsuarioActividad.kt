c3de58c14519ade84c41f81e54c1cd3036103ba3
AlexGonz27 <alexagustingonzalez@gmail.com>
2024-10-27 23:44:50 -0300

    Nueva pantalla

--- app/src/main/java/com/example/demotanque/datosUsuarioActividad.kt
+++ app/src/main/java/com/example/demotanque/datosUsuarioActividad.kt
@@ -8,21 +8,20 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity2 : AppCompatActivity() {
+class datosUsuarioActividad : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main2)
+        setContentView(R.layout.activity_datos_usuario_actividad2)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
     }
-
     fun openMain(view: View)
     {
-        val intent = Intent(this,datosUsuarioActividad::class.java)
+        val intent = Intent(this,MainActivity::class.java)
         startActivity(intent)
     }
 }
